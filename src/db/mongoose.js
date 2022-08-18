75574529586a3b3798508a2be4a7aff7b083d7b8
Atalay Küçükoğlu <atalaykucukoglu@yahoo.com>
2022-08-18 18:28:26 +0300

    mongoose library write operations

---
+++ src/db/mongoose.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { User, Task } = require('./models')
+
+const connectionUrl = "mongodb://127.0.0.1:27017/task-manager-api"
+mongoose.connect(connectionUrl)
+
+// const newUser = new User({
+//   name: '      boslukluisim      ',
+//   // age: 27,
+//   email: '     asdasd@assdasd.com         '
+// })
+
+// newUser.save()
+// .then(() => {
+//   console.log("Saved:", newUser)
+// })
+// .catch(error => {
+//   console.log("could not save", error)
+// })
+
+const newTask = new Task({
+  description: 'Learn mongoose',
+  // completed: false,
+})
+
+newTask.save()
+.then(() => {
+  console.log("Saved: ", newTask)
+})
+.catch(error => {
+  console.log("could not save: ", error)
+})
