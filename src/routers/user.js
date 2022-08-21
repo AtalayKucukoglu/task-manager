85547672b513d2993b4654f86e5f3b87e78b84b2
Atalay Küçükoğlu <atalaykucukoglu@yahoo.com>
2022-08-22 00:57:11 +0300

    remove users api works with user id & force authentication on update and delete user

--- src/routers/user.js
+++ src/routers/user.js
@@ -53,33 +53,38 @@ router.post('/users/logoutAll', auth, async (req, res) => {
   }
  })
 
-// get all users
-router.get('/users', auth, (req, res) => {
-  User.find({}).then((users) => {
-    res.send(users)
-  }).catch((err) => {
-    res.status(400).send(err)
-  });
+// get user profile
+router.get('users/me', auth, async (req, res) => {
+  res.send(req.user)
 })
+
+// get all users
+// router.get('/users', auth, (req, res) => {
+//   User.find({}).then((users) => {
+//     res.send(users)
+//   }).catch((err) => {
+//     res.status(400).send(err)
+//   });
+// })
 
 // get user by id
-router.get('/users/:id', (req, res) => {
-  const { id } = req.params
-  User.findById(id).then((user) => {
-    if (!user) {
-      res.status(404).send()
-    }
+// router.get('/users/:id', (req, res) => {
+//   const { id } = req.params
+//   User.findById(id).then((user) => {
+//     if (!user) {
+//       res.status(404).send()
+//     }
 
-    res.send(user)
-  }).catch((err) => {
-    res.status(400).send(err)
-  });
-})
+//     res.send(user)
+//   }).catch((err) => {
+//     res.status(400).send(err)
+//   });
+// })
 
 // update user by id
-router.patch('/users/:id', async (req, res) => {
-  const { id } = req.params
-  const updates = Object.keys(req.body)
+router.patch('/users/me', auth, async (req, res) => {
+  const { user, body } = req
+  const updates = Object.keys(body)
   const allowedUpdates = ['name', 'email', 'age', 'password']
   const isValidUpdate = updates.every(update => allowedUpdates.includes(update))
   if (!isValidUpdate) {
@@ -88,9 +93,7 @@ router.patch('/users/:id', async (req, res) => {
 
   try {
     // not using findByIdAndUpdate to be able to run pre save function
-    let user = await User.findById(id)
-    updates.forEach(update => user[update] = req.body[update])
-    console.log(user)
+    updates.forEach(update => user[update] = body[update])
     await user.save()
     res.send(user)
   } catch (error) {
@@ -100,18 +103,14 @@ router.patch('/users/:id', async (req, res) => {
 })
 
 // delete user by id
-router.delete('/users/:id', (req, res) => {
-  const { id } = req.params
-  User.findByIdAndDelete(id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send()
-      }
-      res.send(user)
-    })
-    .catch(err => {
-      res.status(500).send(err)
-    })
+router.delete('/users/me', auth, async (req, res) => {
+  try {
+    await req.user.remove()
+    res.send(req.user)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
 })
 
 module.exports = router
